e83b6247dab3f7cd45d45fa444feb548c5bb266f
Chris <chrisem816@gmail.com>
2019-01-13 12:23:53 -0800

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+//timer function
+var time=120;
+$("#display").text("Time remaining: " + time);
+var intervalId;
+function countDown() {
+    time--;
+    $("#display").text("Time remaining: " + time);
+}
+intervalId=setInterval(countDown, 1000);
 
 
 
 
 
-
-    
 });
